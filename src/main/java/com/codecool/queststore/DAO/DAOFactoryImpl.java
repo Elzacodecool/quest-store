782016cf9d9462f190f14d440401d4caa75bb6c3
Elzacodecool <elliegolec@gmail.com>
2018-06-06 14:31:25 +0200

    Add login data

--- src/main/java/com/codecool/queststore/DAO/DAOFactoryImpl.java
+++ src/main/java/com/codecool/queststore/DAO/DAOFactoryImpl.java
@@ -6,11 +6,11 @@ public class DAOFactoryImpl extends DAOFactory {
     private Connection connection;
     private static final String DRIVER = "org.postgresql.Driver";
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/";
-    private static final String DB_NAME = "";
-    private static final String userName = "";
-    private static final String password = "";
+    private static final String DB_NAME = "queststore";
+    private static final String userName = "codecooler";
+    private static final String password = "123";
 
-    DAOFactoryImpl() {
+    public DAOFactoryImpl() {
         this.connection = createConnection();
     }
 
