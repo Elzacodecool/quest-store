bad93b3d5b7f39b74a8b58e3ec6689d8493ff922
wluka85 <wluka85@gmail.com>
2018-06-06 11:32:01 +0200

    Delete unused files

--- src/main/java/com/codecool/queststore/model/Student.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.queststore.model;
-
-
-public class Student extends Codecooler {
-
-    public Student(String name, String surName, String login, String password) {
-        super(name, surName, login, password);
-    }
-}
