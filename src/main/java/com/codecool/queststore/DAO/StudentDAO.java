f3a640546658845c5ad28a339cf90aae6caa8b01
Elzacodecool <elliegolec@gmail.com>
2018-06-07 11:27:20 +0200

    Add methods to studentDAO

--- src/main/java/com/codecool/queststore/DAO/StudentDAO.java
+++ src/main/java/com/codecool/queststore/DAO/StudentDAO.java
@@ -1,4 +1,16 @@
 package com.codecool.queststore.DAO;
 
+import com.codecool.queststore.model.classRoom.ClassRoom;
+import com.codecool.queststore.model.user.Student;
+
+import java.util.List;
+
 public interface StudentDAO {
+    void add(Student student);
+    void remove(Student student);
+    void update(Student student);
+    Student getStudent(int id);
+    List<Student> getStudentsByRoom(ClassRoom classRoom);
+    List<Student> getAllStudents(Student student);
+
 }
