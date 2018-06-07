38b734a5820755c419f4736763e9a0dd38d9bddc
wluka85 <wluka85@gmail.com>
2018-06-07 10:25:41 +0200

    Hotfix in Item class

--- src/main/java/com/codecool/queststore/model/inventory/Item.java
+++ src/main/java/com/codecool/queststore/model/inventory/Item.java
@@ -7,4 +7,48 @@ public class Item {
     private String description;
     private int price;
     private Category category;
+
+    public Item(int id, String name, String description, int price, Category category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
