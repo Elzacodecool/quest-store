6526bdfb0b56cf3fb9ee7d9ee8dfcdae19ccd03f
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-06-07 11:47:08 +0200

    Add 2nd constructor for ClassRoom class

--- src/main/java/com/codecool/queststore/model/classRoom/ClassRoom.java
+++ src/main/java/com/codecool/queststore/model/classRoom/ClassRoom.java
@@ -2,11 +2,15 @@ package com.codecool.queststore.model.classRoom;
 
 public class ClassRoom {
 
-    private int id;
+    private Integer id;
     private String className;
 
     public ClassRoom(int id, String className) {
         this.id = id;
+        this.className = className;
+    }
+
+    public ClassRoom(String className) {
         this.className = className;
     }
 
