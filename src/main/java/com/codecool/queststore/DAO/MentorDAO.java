9701a9b9f5dcec3f63e4c03ed73b5df47599ba08
Elzacodecool <elliegolec@gmail.com>
2018-06-07 11:26:41 +0200

    Add methods to mentorDAO

--- src/main/java/com/codecool/queststore/DAO/MentorDAO.java
+++ src/main/java/com/codecool/queststore/DAO/MentorDAO.java
@@ -1,4 +1,13 @@
 package com.codecool.queststore.DAO;
 
+import com.codecool.queststore.model.user.Mentor;
+
+import java.util.List;
+
 public interface MentorDAO {
+    void add(Mentor mentor);
+    void remove(Mentor mentor);
+    void update(Mentor mentor);
+    Mentor getMentor(int id);
+    List<Mentor> getAllMentors(Mentor mentor);
 }
