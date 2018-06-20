ba043d08b9a8dbc5764633dbfe9d00271d484c9a
Roman <adam.romanowski93@gmail.com>
2018-06-20 17:38:16 +0200

    Add script to change colors on site

---
+++ front/menus/script.js
@@ -0,0 +1,9 @@
+function changeColors() {
+	if (document.body.title == "black") {
+		document.body.style = "background-color: #FFFFFF; color: #000000;";
+		document.body.title = "white";
+	} else {
+		document.body.style = "background-color: #303030; color: #F0F0F0;";
+		document.body.title = "black";
+	}
+}
