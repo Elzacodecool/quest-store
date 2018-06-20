507fc6eacceb6ed9fdcddbb139dd3b190273e799
Roman <adam.romanowski93@gmail.com>
2018-06-20 20:35:33 +0200

    Remove changing theme button with script and title of body

--- front/menus/script.js
+++ front/menus/script.js
@@ -1,13 +1,3 @@
-function changeColors() {
-	if (document.body.title == "black") {
-		document.body.style = "background-color: #FFFFFF; color: #000000;";
-		document.body.title = "white";
-	} else {
-		document.body.style = "background-color: #303030; color: #F0F0F0;";
-		document.body.title = "black";
-	}
-}
-
 function createTiles(tiles) {
 
 	var TITLE_INDEX = 0;
