a1a74b8f468af6623c23539f001f2d8f99d4800f
Roman <adam.romanowski93@gmail.com>
2018-06-20 19:58:41 +0200

    Add scripts creating tiles with class names and content

--- front/menus/script.js
+++ front/menus/script.js
@@ -7,3 +7,52 @@ function changeColors() {
 		document.body.title = "black";
 	}
 }
+
+function createTiles(tilesNamesArray) {
+
+	var TITLE = 0;
+	var TILE_TYPE = 1;
+
+	var mainDiv = document.getElementById("content-container");
+	for (var i = 0; i < tilesNamesArray.length; i++) {
+		
+		var tile = document.createElement("div");
+		var className = getClassName(tilesNamesArray[i][TILE_TYPE]);
+		
+		tile.setAttribute("class", className);
+		
+		var paragraph = document.createElement("p");
+		paragraph.innerHTML = tilesNamesArray[i][TITLE];
+		tile.appendChild(paragraph);
+		
+		mainDiv.appendChild(tile);
+	}
+}
+
+function getClassName(tileType) {
+	var USER = "User";
+	var USER_CLASS = "user-tiles";
+
+	var ARTIFACT = "Artifact";
+	var ARTIFACT_CLASS = "artifact-tiles";
+
+	var QUEST = "Quest";
+	var QUEST_CLASS = "quest-tiles";
+
+	var LOGOUT = "Logout";
+	var LOGOUT_CLASS = "logout-tiles";
+
+	var className = "tile ";
+
+	if (tileType == USER) {
+		className = className + USER_CLASS;
+	} else if (tileType == ARTIFACT) {
+		className = className + ARTIFACT_CLASS;
+	} else if (tileType == QUEST) {
+		className = className + QUEST_CLASS;
+	} else if (tileType == LOGOUT) {
+		className = className + LOGOUT_CLASS;
+	}
+
+	return className;
+}
