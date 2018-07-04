ff972ccadf354393cb65fff16e17af8864172f40
Roman <adam.romanowski93@gmail.com>
2018-07-04 13:42:10 +0200

    Implement session expired case

--- src/main/java/com/codecool/queststore/controller/AdminController.java
+++ src/main/java/com/codecool/queststore/controller/AdminController.java
@@ -6,6 +6,7 @@ import com.codecool.queststore.DAO.DAOFactory;
 import com.codecool.queststore.DAO.DAOFactoryImpl;
 import com.codecool.queststore.model.SingletonAcountContainer;
 import com.codecool.queststore.model.user.Admin;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import org.jtwig.JtwigModel;
@@ -14,12 +15,39 @@ import org.jtwig.JtwigTemplate;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpCookie;
+import java.net.URI;
 
 public class AdminController implements HttpHandler {
+
+    private Admin admin;
+    private URI uri;
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        String response = getResponse(httpExchange);
-        sendResponse(httpExchange, response);
+
+        String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
+        String sessionId = getSessionIdbyCookie(cookieStr);
+
+        if (sessionExpired(sessionId)) {
+            redirect(httpExchange, "index");
+        }
+        
+    }
+
+    private String getSessionIdbyCookie(String cookieStr) {
+        HttpCookie httpCookie = HttpCookie.parse(cookieStr).get(0);
+        return httpCookie.toString().split("=")[1];
+    }
+
+    private boolean sessionExpired(String sessionId) {
+        return sessionId == null;
+    }
+
+    private void redirect(HttpExchange httpExchange, String location) throws IOException {
+        Headers headers = httpExchange.getResponseHeaders();
+        headers.add("Location", location);
+        httpExchange.sendResponseHeaders(302, -1);
+        httpExchange.close();
     }
 
     private String getResponse(HttpExchange httpExchange) {
