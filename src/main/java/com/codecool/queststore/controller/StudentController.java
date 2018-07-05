224e3db36431210192d7bede023b57897ddf58db
Roman <adam.romanowski93@gmail.com>
2018-07-05 10:03:15 +0200

    Add field, extract sending response to foreign method

--- src/main/java/com/codecool/queststore/controller/StudentController.java
+++ src/main/java/com/codecool/queststore/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.codecool.queststore.controller;
 
+import com.codecool.queststore.model.user.Student;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -7,9 +8,17 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public class StudentController implements HttpHandler {
+
+    private Student student;
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        String response = "Hello Student!";
+        
+        String response = "";
+        sendResponse(httpExchange, response);
+    }
+
+    private void sendResponse(HttpExchange httpExchange, String response) throws IOException {
         httpExchange.sendResponseHeaders(200, response.length());
         OutputStream os = httpExchange.getResponseBody();
         os.write(response.getBytes());
