4b4fda48c2886126fae6d6c0e968ba1187a88ef1
Roman <adam.romanowski93@gmail.com>
2018-07-05 10:19:54 +0200

    Implement redirecting if session expired and creating student

--- src/main/java/com/codecool/queststore/controller/StudentController.java
+++ src/main/java/com/codecool/queststore/controller/StudentController.java
@@ -1,11 +1,16 @@
 package com.codecool.queststore.controller;
 
+import com.codecool.queststore.DAO.DAOFactoryImpl;
+import com.codecool.queststore.DAO.StudentDAOImpl;
+import com.codecool.queststore.model.SingletonAcountContainer;
 import com.codecool.queststore.model.user.Student;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.HttpCookie;
 
 public class StudentController implements HttpHandler {
 
@@ -13,9 +18,60 @@ public class StudentController implements HttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        
+
+        redirectToLoginPageIfSessionExpired(httpExchange);
+
+        this.student = getStudentByCookie(httpExchange);
+
         String response = "";
         sendResponse(httpExchange, response);
+    }
+
+    private void redirectToLoginPageIfSessionExpired(HttpExchange httpExchange) throws IOException {
+        String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
+        String sessionId = getSessionIdbyCookie(cookieStr);
+
+        if (sessionExpired(sessionId)) { redirect(httpExchange, "index"); }
+    }
+
+    private String getSessionIdbyCookie(String cookieStr) {
+        HttpCookie httpCookie = HttpCookie.parse(cookieStr).get(0);
+        return httpCookie.toString().split("=")[1];
+    }
+
+    private boolean sessionExpired(String sessionId) {
+        return sessionId == null;
+    }
+
+    private void redirect(HttpExchange httpExchange, String location) throws IOException {
+        Headers headers = httpExchange.getResponseHeaders();
+        headers.add("Location", location);
+        httpExchange.sendResponseHeaders(302, -1);
+        httpExchange.close();
+    }
+
+    private Student getStudentByCookie(HttpExchange httpExchange) {
+        String sessionId = getSessionId(httpExchange);
+        int codecoolerId = getCodecoolerId(sessionId);
+        StudentDAOImpl studentDAO = getStudentDao();
+        return studentDAO.getStudent(codecoolerId);
+    }
+
+    private String getSessionId(HttpExchange httpExchange) {
+        HttpCookie httpCookie;
+        String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
+        httpCookie = HttpCookie.parse(cookieStr).get(0);
+        return httpCookie.toString().split("=")[1];
+    }
+
+    private int getCodecoolerId(String sessionId) {
+        SingletonAcountContainer sessionsIDs = SingletonAcountContainer.getInstance();
+        return sessionsIDs.getCodecoolerId(sessionId);
+    }
+
+    private StudentDAOImpl getStudentDao() {
+        DAOFactoryImpl daoFactory = new DAOFactoryImpl();
+        return new StudentDAOImpl(daoFactory);
     }
 
     private void sendResponse(HttpExchange httpExchange, String response) throws IOException {
