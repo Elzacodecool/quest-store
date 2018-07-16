74487025ee8a023c436b73fb668c53ca45d8a216
szymslow12 <szym.slow12@gmail.com>
2018-07-16 22:53:50 +0200

    fix error

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS codecooler, admin, mentor, student, mentor_class, class, transaction, inventory, item CASCADE;
+﻿DROP TABLE IF EXISTS codecooler, admin, mentor, student, mentor_class, class, transaction, inventory, item CASCADE;
 
 CREATE TABLE codecooler (
 	codecooler_id serial PRIMARY KEY,
@@ -76,7 +76,7 @@ CREATE TABLE item (
     category text
 );
 
-INSERT INTO item (name, decription, price, category) 
+INSERT INTO item (name, description, price, category) 
 VALUES ('name', 'description', 25, 'single');
 
 CREATE TABLE transaction (
