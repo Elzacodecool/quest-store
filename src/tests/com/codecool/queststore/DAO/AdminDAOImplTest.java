b096b782af2db9f2344d8e0170f713dd330726b0
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-07-17 14:11:19 +0200

    implement shouldReturnAdminList_4Items test

--- src/tests/com/codecool/queststore/DAO/AdminDAOImplTest.java
+++ src/tests/com/codecool/queststore/DAO/AdminDAOImplTest.java
@@ -31,12 +31,13 @@ public class AdminDAOImplTest {
     public void setUp() throws Exception {
         UserDetails ud = new UserDetails("FirstName", "LastName", "Email", "Login", "Pasword", "AccountType");
         when(daoFactory.execQuery(any(String.class), any(Integer.class))).thenReturn(rs);
-        when(rs.next()).thenReturn(true);
+        when(daoFactory.execQuery(any(String.class))).thenReturn(rs);
         when(userDAO.getUser(any(Integer.class))).thenReturn(ud);
         when(daoFactory.getUserDAO()).thenReturn(userDAO);
     }
 
     private void setUpForReturnAdmin() throws Exception {
+        when(rs.next()).thenReturn(true);
         when(rs.getInt(any(String.class))).thenReturn(1);
     }
 
@@ -62,5 +63,21 @@ public class AdminDAOImplTest {
     public void shouldReturnNull() throws Exception {
         setUpForNull();
         assertEquals(new AdminDAOImpl(daoFactory).getAdmin(1), null);
+    }
+
+    private void setUpForReturnAdminList() throws Exception {
+        when(rs.next()).thenReturn(true).thenReturn(true).thenReturn(true).thenReturn(true).thenReturn(false);
+        when(rs.getInt(any(String.class))).thenReturn(1).thenReturn(2).thenReturn(3).thenReturn(4);
+        UserDetails ud1 = new UserDetails("E", "K", "E", "K", "R", "S");
+        UserDetails ud2 = new UserDetails("M", "R", "Z", "A", "B", "C");
+        UserDetails ud3 = new UserDetails("S", "L", "E", "K", "R", "S");
+        UserDetails ud4 = new UserDetails("K", "O", "Z", "A", "B", "C");
+
+        when(userDAO.getUser(any(Integer.class))).thenReturn(ud1).thenReturn(ud2).thenReturn(ud3).thenReturn(ud4);
+    }
+    @Test
+    public void shouldReturnAdminList_4Items() throws Exception {
+        setUpForReturnAdminList();
+        assertEquals(4, new AdminDAOImpl(daoFactory).getAllAdmins().size());
     }
 }
