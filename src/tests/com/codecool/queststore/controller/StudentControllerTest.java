1347a5415e209ef56e1cc62204d5a9f81802de90
szymslow12 <szym.slow12@gmail.com>
2018-07-19 20:40:26 +0200

    delete unused import, update documentation

--- src/tests/com/codecool/queststore/controller/StudentControllerTest.java
+++ src/tests/com/codecool/queststore/controller/StudentControllerTest.java
@@ -20,7 +20,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
