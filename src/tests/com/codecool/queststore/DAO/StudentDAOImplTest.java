a4251a998a54896d688d21fa47b27974d9d8f5d4
szymslow12 <szym.slow12@gmail.com>
2018-07-19 16:56:39 +0200

    delete unused imports

--- src/tests/com/codecool/queststore/DAO/StudentDAOImplTest.java
+++ src/tests/com/codecool/queststore/DAO/StudentDAOImplTest.java
@@ -7,25 +7,21 @@ import com.codecool.queststore.model.inventory.Inventory;
 import com.codecool.queststore.model.inventory.Item;
 import com.codecool.queststore.model.user.Student;
 import com.codecool.queststore.model.user.UserDetails;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.mockito.Mock;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyChar;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 
