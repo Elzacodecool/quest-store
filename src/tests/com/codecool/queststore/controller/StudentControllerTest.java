b2d226d09f3df70f2c060c3969fee65c47efa183
szymslow12 <szym.slow12@gmail.com>
2018-07-19 17:01:41 +0200

    delete unused imports in StudentControllerTest

--- src/tests/com/codecool/queststore/controller/StudentControllerTest.java
+++ src/tests/com/codecool/queststore/controller/StudentControllerTest.java
@@ -1,34 +1,24 @@
 package com.codecool.queststore.controller;
 
 import com.codecool.queststore.model.SingletonAcountContainer;
-import com.codecool.queststore.model.user.UserDetails;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpServer;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.mockito.Mock;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 
